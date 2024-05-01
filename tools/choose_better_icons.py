3c35c282e071a10aeb4ceccb65e69bfda8d70ece
da-qing-wa <qiweid@sairlab.org>
2024-05-01 08:45:56 -0400

    minor fix

--- tools/choose_better_icons.py
+++ tools/choose_better_icons.py
@@ -16,7 +16,6 @@ for type_name in os.listdir(original_dir_1):
     if not os.path.exists(type_dir_new):
         os.makedirs(type_dir_new)
 
-    idx = 0
     for img_name in os.listdir(type_dir_1):
         im_1 = cv2.imread(os.path.join(type_dir_1, img_name), cv2.IMREAD_UNCHANGED)
         im_2 = cv2.imread(os.path.join(type_dir_2, img_name), cv2.IMREAD_UNCHANGED)
@@ -27,9 +26,8 @@ for type_name in os.listdir(original_dir_1):
         # the more non-transparent pixels, the better
         if np.sum(im_1[:, :, 3])//255 > np.sum(im_2[:, :, 3])//255:
             # print("im_1")
-            cv2.imwrite(os.path.join(type_dir_new, f"image_{idx}.png"), im_1)
+            cv2.imwrite(os.path.join(type_dir_new, img_name), im_1)
         else:
             # print("im_2")
-            cv2.imwrite(os.path.join(type_dir_new, f"image_{idx}.png"), im_2)
+            cv2.imwrite(os.path.join(type_dir_new, img_name), im_2)
 
-        idx += 1
