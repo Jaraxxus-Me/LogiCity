47afb02832709087d78e4cb85bd6629343cfb5bf
da-qing-wa <qiweid@sairlab.org>
2024-05-03 12:12:37 -0400

    add bilevel, unrolled finished, implicit not finished

--- scripts/vis/easy/test_all.sh
+++ scripts/vis/easy/test_all.sh
@@ -1,18 +1,6 @@
 source /opt/conda/etc/profile.d/conda.sh
 conda activate logicity
 
-# python tools/test_vis_input.py --config config/tasks/Vis/ResNetNLM/easy_200_fixed_e2e.yaml \
-#     --ckpt vis_input_weights/easy/easy_200_fixed_nlm_e2e/easy_200_fixed_nlm_e2e_best.pth \
-#     >> log_sim/easy_200_fixed_nlm_e2e_test.log
-
-# python tools/test_vis_input.py --config config/tasks/Vis/ResNetGNN/easy_200_fixed_e2e.yaml \
-#     --ckpt vis_input_weights/easy/easy_200_fixed_gnn_e2e/easy_200_fixed_gnn_e2e_best.pth \
-#     >> log_sim/easy_200_fixed_gnn_e2e_test.log
-
-# python tools/test_vis_input.py --config config/tasks/Vis/ResNetNLM/easy_200_fixed_modular.yaml \
-#     --ckpt vis_input_weights/easy/easy_200_fixed_nlm_modular/easy_200_fixed_nlm_modular_best.pth \
-#     >> log_sim/easy_200_fixed_nlm_modular_test.log
-
 
 python3 tools/test_vis_input.py --config config/tasks/Vis/ResNetNLM/easy_100_fixed_demo_modular.yaml \
     --ckpt vis_input_weights/easy/easy_100_fixed_demo_nlm_modular/easy_100_fixed_demo_nlm_modular_best.pth \
@@ -29,3 +17,7 @@ python3 tools/test_vis_input.py --config config/tasks/Vis/ResNetNLM/easy_100_fix
 python3 tools/test_vis_input.py --config config/tasks/Vis/ResNetNLM/easy_100_fixed_no_variance_e2e.yaml \
     --ckpt vis_input_weights/easy/easy_100_fixed_no_variance_nlm_e2e/easy_100_fixed_no_variance_nlm_e2e_best.pth \
     >> log_sim/easy_100_fixed_no_variance_nlm_e2e_test.log
+
+python3 tools/test_vis_input.py --config config/tasks/Vis/ResNetNLM/easy_100_fixed_demo_bilevel.yaml \
+    --ckpt vis_input_weights/easy/easy_100_fixed_demo_bilevel_unrolled/easy_100_fixed_demo_bilevel_unrolled_best.pth \
+    >> log_sim/easy_100_fixed_demo_bilevel_unrolled_test.log
--- scripts/vis/easy/train_all.sh
+++ scripts/vis/easy/train_all.sh
@@ -1,15 +1,6 @@
 source /opt/conda/etc/profile.d/conda.sh
 conda activate logicity
 
-# python3 tools/train_vis_input.py --config config/tasks/Vis/ResNetNLM/easy_200_fixed_modular.yaml \
-#     --exp easy_200_fixed_nlm_modular \
-#     --modular >> log_sim/easy_200_fixed_nlm_modular.log
-
-# python3 tools/train_vis_input.py --config config/tasks/Vis/ResNetNLM/easy_200_fixed_e2e.yaml \
-#     --exp easy_200_fixed_nlm_e2e >> log_sim/easy_200_fixed_nlm_e2e.log
-
-# python3 tools/train_vis_input.py --config config/tasks/Vis/ResNetGNN/easy_200_fixed_e2e.yaml \
-#     --exp easy_200_fixed_gnn_e2e >> log_sim/easy_200_fixed_gnn_e2e.log
 
 python3 tools/train_vis_input.py --config config/tasks/Vis/ResNetNLM/easy_100_fixed_demo_modular.yaml \
     --exp easy_100_fixed_demo_nlm_modular \
@@ -24,3 +15,10 @@ python3 tools/train_vis_input.py --config config/tasks/Vis/ResNetNLM/easy_100_fi
 
 python3 tools/train_vis_input.py --config config/tasks/Vis/ResNetNLM/easy_100_fixed_no_variance_e2e.yaml \
     --exp easy_100_fixed_no_variance_nlm_e2e >> log_sim/easy_100_fixed_no_variance_nlm_e2e.log
+
+python3 tools/train_vis_input_bilevel.py --config config/tasks/Vis/ResNetNLM/easy_100_fixed_demo_bilevel.yaml \
+    --exp easy_100_fixed_demo_bilevel_unrolled >> log_sim/easy_100_fixed_demo_bilevel_unrolled.log
+
+python3 tools/train_vis_input_bilevel.py --config config/tasks/Vis/ResNetNLM/easy_100_fixed_demo_bilevel.yaml \
+    --exp easy_100_fixed_demo_bilevel_implicit \
+    --implicit >> log_sim/easy_100_fixed_demo_bilevel_implicit.log
