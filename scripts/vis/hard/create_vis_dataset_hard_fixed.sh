a948efdf8bee24db03495eb92698538195c2a833
Bowen Li <bowenli2@andrew.cmu.edu>
2024-05-08 13:18:35 -0400

    1st version of vis dataset

--- scripts/vis/hard/create_vis_dataset_hard_fixed.sh
+++ scripts/vis/hard/create_vis_dataset_hard_fixed.sh
@@ -1,14 +1,14 @@
 source /opt/conda/etc/profile.d/conda.sh
 conda activate logicity
 
-EXPNAME="hard_100_fixed"
+EXPNAME="hard_fixed_final"
 python3 main.py \
         --config config/tasks/Vis/hard.yaml \
         --mode hard \
         --exp ${EXPNAME} \
-        --train_world_num 40 \
-        --val_world_num 10 \
-        --test_world_num 10 \
+        --train_world_num 100 \
+        --val_world_num 20 \
+        --test_world_num 20 \
         --dataset_dir vis_dataset/${EXPNAME} \
         --max_steps 100 \
         --create_vis_dataset
