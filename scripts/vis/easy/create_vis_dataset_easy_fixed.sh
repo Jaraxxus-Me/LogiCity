3d041bb44ffc4bcdd7fdda93b87790e6a5ba343f
Bowen Li <bowenli2@andrew.cmu.edu>
2024-05-15 22:23:47 -0400

    latest vis

--- scripts/vis/easy/create_vis_dataset_easy_fixed.sh
+++ scripts/vis/easy/create_vis_dataset_easy_fixed.sh
@@ -11,14 +11,14 @@ conda activate logicity
 # EXPNAME="easy_100_fixed_no_variance"
 
 ### use this EXPNAME for a temp demo ###
-EXPNAME="easy_fixed_final"
+EXPNAME="demo_fixed_final"
 python3 main.py \
-        --config config/tasks/Vis/easy.yaml \
-        --mode easy \
+        --config config/tasks/Vis/demo.yaml \
+        --mode demo \
         --exp ${EXPNAME} \
-        --train_world_num 100 \
-        --val_world_num 20 \
-        --test_world_num 20 \
+        --train_world_num 10 \
+        --val_world_num 5 \
+        --test_world_num 5 \
         --dataset_dir vis_dataset/${EXPNAME} \
-        --max_steps 100 \
+        --max_steps 80 \
         --create_vis_dataset
