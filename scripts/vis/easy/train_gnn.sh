ece06055a450132738e2e01a1459b622195a28f9
Bowen Li <bowenli2@andrew.cmu.edu>
2024-06-02 10:50:16 -0400

    submission for VAP

--- scripts/vis/easy/train_gnn.sh
+++ scripts/vis/easy/train_gnn.sh
@@ -2,9 +2,9 @@ source /opt/conda/etc/profile.d/conda.sh
 conda activate base
 
 
-python3 tools/train_vis_input_gnn.py --config config/tasks/Vis/ResNetGNN/easy_200_fixed_e2e.yaml --exp easy_200_fixed_e2e_gnn >> log_vis/easy_200_fixed_e2e_gnn.log
+python3 tools/train_vis_input_gnn.py --config config/tasks/Vis/ResNetGNN/veryeasy_200_fixed_e2e.yaml --exp veryeasy_200_fixed_e2e_gnn >> log_vis/veryeasy_200_fixed_e2e_gnn.log
 
-python3 tools/train_vis_input_gnn.py --config config/tasks/Vis/ResNetGNN/easy_200_fixed_modular2.yaml --modular --exp easy_200_fixed_modular_gnn2 >> log_vis/easy_200_fixed_modular_gnn2.log
+python3 tools/train_vis_input_gnn.py --config config/tasks/Vis/ResNetGNN/veryeasy_200_fixed_modular2.yaml --modular --exp veryeasy_200_fixed_modular_gnn2 >> log_vis/veryeasy_200_fixed_modular_gnn2.log
 
 python3 tools/train_vis_input_gnn.py --config config/tasks/Vis/ResNetGNN/hard_200_fixed_e2e.yaml --exp hard_200_fixed_e2e_gnn >> log_vis/hard_200_fixed_e2e_gnn.log
 
